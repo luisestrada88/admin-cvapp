597408eea3dc00cdd826c88b49cd57189aa9ee22
Ander Luna <zS23020596@estudiantes.uv.mx>
2025-04-08 19:14:28 -0600

    agregación de gitignore

---
+++ .angular/cache/18.2.15/admin-cv/vite/deps/@angular_fire_compat.js
@@ -0,0 +1,25 @@
+import {
+  AngularFireModule,
+  FIREBASE_APP_NAME,
+  FIREBASE_OPTIONS,
+  FirebaseApp,
+  ɵapplyMixins,
+  ɵcacheInstance,
+  ɵfirebaseAppFactory,
+  ɵlazySDKProxy
+} from "./chunk-OG5KL2FU.js";
+import "./chunk-IYKWNOWL.js";
+import "./chunk-YDWOA34S.js";
+import "./chunk-CHD3SIRK.js";
+import "./chunk-XY4I24L7.js";
+export {
+  AngularFireModule,
+  FIREBASE_APP_NAME,
+  FIREBASE_OPTIONS,
+  FirebaseApp,
+  ɵapplyMixins,
+  ɵcacheInstance,
+  ɵfirebaseAppFactory,
+  ɵlazySDKProxy
+};
+//# sourceMappingURL=@angular_fire_compat.js.map
---
+++ .angular/cache/18.2.15/admin-cv/vite/deps/index.esm-ZMLMWAGA.js
@@ -0,0 +1,4 @@
+import "./chunk-2D66RMQP.js";
+import "./chunk-IYKWNOWL.js";
+import "./chunk-XY4I24L7.js";
+//# sourceMappingURL=index.esm-ZMLMWAGA.js.map
